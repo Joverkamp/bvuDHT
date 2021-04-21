0b511a81f8cedb73398058ba283d1f30b39c0f83
HullJac <jacobhull55@gmail.com>
2021-04-21 15:14:46 -0500

    Did some commenting of the code so far.

--- bvuDHT-client.py
+++ bvuDHT-client.py
@@ -7,19 +7,29 @@ from pathlib import Path
 import threading
 import os
 
+
+# Global variables we want to keep track of
 MY_ADDR = ''
 SUCC_ADDR = ''
 PRED_ADDR = ''
+# How many fingers we want people in the system to have
 NUM_FINGERS = 4
+# The finger table is a list of all the people we know.
+# It contains our own address, our predacessor's address, 
+# our successor's address, and NUM_FINGERS addresses of people 
+# closeset to offests of the circle's size divided NUM_FINGERS.
 FINGER_TABLE = []
+# A list of commands that the user is allowed to use
 COMMANDS = ['contains', 'insert', 'remove', 'disconnect', 'help']
 
 
+# Returns us a hashed value of the string 
 def getHashKey(value):
     key = hashlib.sha1(value.encode()).hexdigest()
     return key
 
 
+# Finds out who we know that is closest to the key
 def closestToKey(key):
     #TODO account for wrap around
     #Put in for loop
@@ -46,15 +56,18 @@ def closestToKey(key):
         return PRED_ADDR;
 
 
+# Helper function that tells us if we are the owner of the file
 def containedLocal(searchString):
     fileKey = getHashKey(searchString)
     
+    # TODO May need to change this to reflect how we do the new finger table
     if MY_ADDR == SUCC_ADDR:
         if fileKey in getMyFileKeys():
             return True
         return False
 
 
+# Finds who is the closest peer to the string we want to search by
 def closestPeer(searchString):
     key = getHashKey(searchString)
     closest = closestToKey(key)
@@ -68,6 +81,8 @@ def closestPeer(searchString):
         #else repeat
         pass
 
+
+# Stores a file in the DHT
 def insert(searchString):
     key = getHashKey(searchString)
     storeAddr = closestPeer(searchString)
@@ -79,17 +94,22 @@ def insert(searchString):
         #network protocol
         pass
 
+
+# Function to call when the user wants to create their own system
 def startNewSystem():
     SUCC_ADDR = MY_ADDR
     PRED_ADDR = MY_ADDR
     for i in range(NUM_FINGERS):
         FINGER_TABLE.append(MY_ADDR)
 
+
+# Returns a list of all the keys we own
 def getMyFileKeys():
     fileKeys = os.listdir('./repository')    
     return fileKeys
 
 
+# Function that prints out all available ways to use the system
 def help():
     print('Here is a list of possible commands...')
     print('contains <file-name>')
@@ -99,23 +119,24 @@ def help():
     print('help')
 
 
-
+# Execution of main code begins here
 if __name__ == '__main__':
     progname = argv[0]
     if len(argv) not in [1, 3]:
         print('Usage: {}')
         print('Usage: {} <IP> <PORT>')
 
-    #set up listener 
+    # Set up listener 
     listener = socket(AF_INET, SOCK_STREAM)
     listener.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     listener.bind(('', 0))
     listeningPort = listener.getsockname()[1]
 
-    #set my address
+    # Set my address
     ip = check_output(['hostname', '-I']).decode().rstrip()
     MY_ADDR = '{}:{}'.format(ip,listeningPort)
 
+    # Check for repository to store files
     folder = Path('./repository')
     if not folder.exists():
         folder.mkdir(parents=True, exist_ok=True)
@@ -123,10 +144,10 @@ if __name__ == '__main__':
     if len(argv) == 1:
         startNewSystem()
     else:
-        # have to get values from person we know in system
+        # Have to get values from person we know in system
         pass
 
-
+    # Main run loop
     running = True
     help()
     while running:
